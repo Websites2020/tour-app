6def202fe8e4f30a3156440814673ff864969150
SOIP <38543876+sumofitsparts@users.noreply.github.com>
2018-08-10 23:57:30 -0400

    design changes part 2

--- public/script.js
+++ public/script.js
@@ -1,22 +1,12 @@
 
-// var btn = document.getElementById(`myBtn${[a]}`);
-// console.log(btn);
-// var span = document.getElementsByClassName("close")[0];
-// var modal = document.getElementById('myModal');
+$('.carousel').carousel({
+    interval: 10,
+    pause: false
+  })
 
-// btn.onclick = function() {
-//     modal.style.display = "block";
-// }
-
-// span.onclick = function() {
-//     modal.style.display = "none";
-// }
-
-// window.onclick = function(event) {
-//     if (event.target == modal) {
-//         modal.style.display = "none";
-//     }
-// }
+function tourReq() {
+    alert("Your tour request has been made.  Thank you.  Hang tight as locals will contact you about it via email.")
+}
 
 function getData() {
     $.get("/show", function(data, status){
